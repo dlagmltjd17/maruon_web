545a0e63e053a14a2faed430a73fffe3c476b4d0
dlagmltjd17 <dlagmltjd17@kakao.com>
2024-02-12 23:48:51 +0900

    Updates

---
+++ precache-manifest.17e3808f8ddd0ea2dffe4544ddbf8276.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f35dc099549bf285d33253a3abdd21db",
+    "url": "/maruon_web/index.html"
+  },
+  {
+    "revision": "7ead35ba220de6e63f9a",
+    "url": "/maruon_web/static/css/main.e7e35eaa.chunk.css"
+  },
+  {
+    "revision": "f1c04d3ebffed598cf5b",
+    "url": "/maruon_web/static/js/2.2a297267.chunk.js"
+  },
+  {
+    "revision": "7ead35ba220de6e63f9a",
+    "url": "/maruon_web/static/js/main.c86793f5.chunk.js"
+  },
+  {
+    "revision": "abf33fc84c3be0a9bd3b",
+    "url": "/maruon_web/static/js/runtime~main.453d87ac.js"
+  }
+]);
